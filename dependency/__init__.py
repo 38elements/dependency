42d89f3a1ced9f18dd7f5928d9753a952c5ed6e6
Tom Christie <tom@tomchristie.com>
2017-07-18 12:49:21 +0100

    Version 0.0.2

--- dependency/__init__.py
+++ dependency/__init__.py
@@ -2,4 +2,4 @@ from dependency.core import Injector, InjectedFunction, ParamName
 from dependency.wrappers import provider, inject, set_required_state
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
